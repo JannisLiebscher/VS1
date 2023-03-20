82cc3532ccffbb0ff98fa80305dccd32edc3d290
Manz2 <erikmanzer21@gmail.com>
2023-03-20 12:17:48 +0100

    aufgabe 2.1

--- src/Broker.java
+++ src/Broker.java
@@ -7,15 +7,68 @@ import aqua.blatt1.common.msgtypes.RegisterResponse;
 import messaging.*;
 
 import java.net.InetSocketAddress;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Broker {
+    private static final int NUM_THREADS = 5;
     private Endpoint endpoint = new Endpoint(4711);
     private ClientCollection clients = new ClientCollection();
     private int counter = 0;
-
+    private ReadWriteLock lock = new ReentrantReadWriteLock();
     public void broker() {
-        while(true) {
+        ExecutorService executor = Executors.newFixedThreadPool(NUM_THREADS);
+        boolean done = false;
+        while(!done) {
             Message message = endpoint.blockingReceive();
+            executor.execute(new BrokerTask(message));
+        }
+    }
+
+    public void register(int port) {
+        lock.writeLock().lock();
+        clients.add("tank" + counter, port);
+        lock.readLock().unlock();
+        endpoint.send(new InetSocketAddress("localhost", port), new RegisterResponse("tank" + counter));
+        System.out.println("Registered tank" + counter);
+        counter++;
+    }
+    public void deregister(int port) {
+        lock.writeLock().lock();
+        clients.remove(clients.indexOf(port));
+        lock.writeLock().unlock();
+    }
+
+    public void handoffFish(int port, FishModel fish) {
+        lock.readLock().lock();
+        int index = clients.indexOf(port);
+        if (fish.getDirection().getVector() < 0) {
+            int neighbour = index == 0 ? clients.size() - 1 : index - 1;
+            int sendTo = (int) clients.getClient(neighbour);
+            lock.readLock().unlock();
+            endpoint.send(new InetSocketAddress("localhost", sendTo), new HandoffRequest(fish));
+        } else {
+            int neighbour = index == clients.size() - 1 ? 0 : index + 1;
+            int sendTo = (int) clients.getClient(neighbour);
+            lock.readLock().unlock();
+            endpoint.send(new InetSocketAddress("localhost", sendTo), new HandoffRequest(fish));
+        }
+    }
+
+    public static void main( String[] args) {
+        Broker broker = new Broker();
+        broker.broker();
+    }
+    public class BrokerTask  implements Runnable {
+        private Message message;
+        BrokerTask(Message message){
+            this.message = message;
+        }
+
+        @Override
+        public void run() {
             int port = message.getSender().getPort();
             if(message.getPayload() instanceof RegisterRequest) register(port);
             else if(message.getPayload() instanceof DeregisterRequest) deregister(port);
@@ -25,34 +78,6 @@ public class Broker {
                 System.out.println("Unsupported Request");
             }
         }
-    }
-
-    public void register(int port) {
-        clients.add("tank" + counter, port);
-        endpoint.send(new InetSocketAddress("localhost", port), new RegisterResponse("tank" + counter));
-        System.out.println("Registered tank" + counter);
-        counter++;
-    }
-    public void deregister(int port) {
-        clients.remove(clients.indexOf(port));
-    }
-
-    public void handoffFish(int port, FishModel fish) {
-        int index = clients.indexOf(port);
-        if (fish.getDirection().getVector() < 0) {
-            int neighbour = index == 0 ? clients.size() - 1 : index - 1;
-            int sendTo = (int) clients.getClient(neighbour);
-            endpoint.send(new InetSocketAddress("localhost", sendTo), new HandoffRequest(fish));
-        } else {
-            int neighbour = index == clients.size() - 1 ? 0 : index + 1;
-            int sendTo = (int) clients.getClient(neighbour);
-            endpoint.send(new InetSocketAddress("localhost", sendTo), new HandoffRequest(fish));
-        }
-    }
-
-    public static void main( String[] args) {
-        Broker broker = new Broker();
-        broker.broker();
     }
 
 }
