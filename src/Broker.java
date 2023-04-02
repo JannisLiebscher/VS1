f31ac330a350b395cb21e678964a5559adbebc3c
Jannis Liebscher <jannis-liebscher@gmx.de>
2023-04-02 18:22:25 +0200

    - Using StopRequest instead of null - Logging Requests

--- src/Broker.java
+++ src/Broker.java
@@ -1,10 +1,7 @@
 import aqua.blatt1.broker.ClientCollection;
 import aqua.blatt1.common.FishModel;
 import aqua.blatt1.common.Properties;
-import aqua.blatt1.common.msgtypes.DeregisterRequest;
-import aqua.blatt1.common.msgtypes.HandoffRequest;
-import aqua.blatt1.common.msgtypes.RegisterRequest;
-import aqua.blatt1.common.msgtypes.RegisterResponse;
+import aqua.blatt1.common.msgtypes.*;
 import aqua.blatt2.broker.PoisonPill;
 import messaging.Endpoint;
 import messaging.Message;
@@ -32,7 +29,7 @@ public class Broker {
             Message message = endpoint.blockingReceive();
             executor.execute(new BrokerTask(message));
         }
-        System.out.println("end of while");
+        System.out.println("Broker exit, killing threads");
         executor.shutdown();
         stop.interrupt();
     }
@@ -42,8 +39,7 @@ public class Broker {
     }
     private void terminate(){
         done = true;
-        endpoint.send(new InetSocketAddress("localhost", Properties.PORT), null);
-        System.out.println("stop ende");
+        endpoint.send(new InetSocketAddress("localhost", Properties.PORT), new StopRequest());
     }
 
 
@@ -80,7 +76,6 @@ public class Broker {
     public static void main( String[] args) {
         Broker broker = new Broker();
         broker.broker();
-        System.out.println("main ende");
     }
     public class BrokerTask  implements Runnable {
         private Message message;
@@ -91,13 +86,26 @@ public class Broker {
         @Override
         public void run() {
             int port = message.getSender().getPort();
-            if(message.getPayload() instanceof RegisterRequest) register(port);
-            else if(message.getPayload() instanceof DeregisterRequest) deregister(port);
-            else if(message.getPayload() instanceof HandoffRequest) handoffFish(port,
-                    ((HandoffRequest) message.getPayload()).getFish());
-            else if(message.getPayload() instanceof PoisonPill) terminate();
+            if(message.getPayload() instanceof RegisterRequest) {
+                System.out.println("Received register Request");
+                register(port);
+            }
+            else if(message.getPayload() instanceof DeregisterRequest) {
+                System.out.println("Received deregister Request");
+                deregister(port);
+            }
+            else if(message.getPayload() instanceof HandoffRequest) {
+                System.out.println("Received handoffFish Request");
+                handoffFish(port, ((HandoffRequest) message.getPayload()).getFish());
+            }
+            else if(message.getPayload() instanceof PoisonPill) {
+                System.out.println("Received Poison Pill");
+                terminate();
+            }
+            else if(message.getPayload() instanceof StopRequest)
+                System.out.println("Received stop request, shutdown");
             else {
-                System.out.println("Unsupported Request or shutdown");
+                System.out.println("Unsupported Request");
             }
         }
     }
